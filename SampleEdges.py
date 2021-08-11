6c83aa680444d2ecd083dd251c6860405196ccc1
Chi-Hung Chang <chihung861224@gmail.com>
2021-08-11 12:54:31 +0800

    use ogb package

--- SampleEdges.py
+++ SampleEdges.py
@@ -8,93 +8,28 @@ from ogb.linkproppred import PygLinkPropPredDataset
 
 # Arguments
 parser = argparse.ArgumentParser(description="OGB Dataset")
+parser.add_argument("--dataset", type=str, default="ogbl-citation2")
 parser.add_argument("--train_network", type=str, default="network.txt")
 parser.add_argument("--train_percent", type=int, default=100)
 args = parser.parse_args()
 
 # Save arguments to variables
-dataset_name = "ogbl-citation2"
-train_path = "train_edges.json"
-valid_path = "valid_edges.json"
-test_path = "test_edges.json"
+dataset_name = args.dataset
 train_network = args.train_network
 train_percent = args.train_percent
 
 # Load dataset from OGB
 dataset = PygLinkPropPredDataset(name=dataset_name)
 split_edge = dataset.get_edge_split()
-train_edge, valid_edge, test_edge = split_edge["train"], split_edge["valid"], split_edge["test"]
-
-# Transform OGB dataset format to json
-train_map = {
-    "source": train_edge["source_node"].tolist(),
-    "target": train_edge["target_node"].tolist()
-}
-valid_map = {
-    "source": valid_edge["source_node"].tolist(),
-    "pos_node": valid_edge["target_node"].tolist(),
-    "neg_node": valid_edge["target_node_neg"].tolist()
-}
-test_map = {
-    "source": test_edge["source_node"].tolist(),
-    "pos_node": test_edge["target_node"].tolist(),
-    "neg_node": test_edge["target_node_neg"].tolist()
-}
-
-train_dict = dict()
-valid_dict = dict()
-test_dict = dict()
-
-print("Making training set....")
-
-for i in tqdm(range(len(train_map["source"]))):
-    source = train_map["source"][i]
-    target = train_map["target"][i]
-
-    try:
-        train_dict[source].append(target)
-    except:
-        train_dict[source] = list()
-        train_dict[source].append(target)
-
-json.dump(train_dict, open(train_path, "w"))
-print("Training set made!!")
-
-print("Making validation set....")
-
-for i in tqdm(range(len(valid_map["source"]))):
-    source = valid_map["source"][i]
-    pos_node = valid_map["pos_node"][i]
-    neg_node = valid_map["neg_node"][i]
-
-    valid_dict[source] = dict()
-    valid_dict[source]["pos_node"] = pos_node
-    valid_dict[source]["neg_node"] = neg_node
-
-json.dump(valid_dict, open(valid_path, "w"))
-print("Validation set made!!")
-
-print("Making testing set....")
-
-for i in tqdm(range(len(test_map["source"]))):
-    source = test_map["source"][i]
-    pos_node = test_map["pos_node"][i]
-    neg_node = test_map["neg_node"][i]
-
-    test_dict[source] = dict()
-    test_dict[source]["pos_node"] = pos_node
-    test_dict[source]["neg_node"] = neg_node
-
-json.dump(test_dict, open(test_path, "w"))
-print("Testing set made!!")
+train_edge = split_edge["train"]
 
 # Sample training network
-print("Start making training network")
+print("Start generating training network")
 network = list()
 
-for i in range(len(train_map["source"])):
-    source = train_map["source"][i]
-    target = train_map["target"][i]
+for idx in range(train_edge["source_node"].size()[0]):
+    source = int(train_edge["source_node"][idx])
+    target = int(train_edge["target_node"][idx])
     network.append([source, target])
 
 num_sample = int(train_percent / 100 * len(network))
--- predict.py
+++ predict.py
@@ -4,17 +4,18 @@ import torch
 import random
 import numpy as np
 from tqdm import tqdm
+from ogb.linkproppred import PygLinkPropPredDataset, Evaluator
 
 # Parse arguments
 parser = argparse.ArgumentParser(description="OGB link prediction task")
-parser.add_argument("--val_edges", type=str, default="valid_edges.json")
-parser.add_argument("--test_edges", type=str, default="test_edges.json")
+parser.add_argument("--dataset", type=str, default="ogbl-citation2")
 parser.add_argument("--val_percent", type=float, default=100)
 parser.add_argument("--test_percent", type=float, default=100)
 parser.add_argument("--embed", type=str, default="ogbl-citation2/node-feat.csv")
 args = parser.parse_args()
 
 embed_path = args.embed
+dataset_name = args.dataset
 val_percent = args.val_percent
 test_percent = args.test_percent
 
@@ -31,89 +32,83 @@ if embed_path.startswith("hpe"):
 
         if len(paper_embed) > 1:
             paper_id = int(paper_id)
-            #paper_embed = np.array([float(e) for e in paper_embed])
-            paper_embed = torch.tensor([float(e) for e in paper_embed])
+            paper_embed = np.array([float(e) for e in paper_embed])
             embedding[paper_id] = paper_embed
 elif embed_path.startswith("ogbl-citation2/node-feat.csv"):
     embedding = np.genfromtxt(embed_path, delimiter=",", skip_header=False)
 else:
     raise ValueError("Unknown embedding file")
 
+# Load validation set and testing set
+dataset = PygLinkPropPredDataset(name=dataset_name)
+split_edge = dataset.get_edge_split()
+valid_edge = split_edge["valid"]
+test_edge = split_edge["test"]
+evaluator = Evaluator(name=dataset_name)
+
 # Setup and initial variables
-num_pos = 1
-num_neg = 1000
-
-val_edges = json.load(open(args.val_edges))
-val_num_sample = int(val_percent / 100 * len(val_edges))
-val_sampled_keys = random.sample(val_edges.keys(), val_num_sample)
-test_edges = json.load(open(args.test_edges))
-test_num_sample = int(test_percent / 100 * len(test_edges))
-test_sampled_keys = random.sample(test_edges.keys(), test_num_sample)
-
-val_mrrs = list()
-test_mrrs = list()
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+val_size = valid_edge["source_node"].size()[0]
+val_num_sample = int(val_percent / 100 * val_size)
+val_sample_index = random.sample(range(val_size), val_num_sample)
+test_size = test_edge["source_node"].size()[0]
+test_num_sample = int(test_percent / 100 * test_size)
+test_sample_index = random.sample(range(test_size), test_num_sample)
 
 # Predict validation set
 print("Predicting validation set....")
+pos_scores = list()
+neg_scores = list()
 
-for key in tqdm(val_sampled_keys):
-    source = int(key)
-    target_nodes = [val_edges[key]["pos_node"]] + val_edges[key]["neg_node"]
-    
-    y_true = [1] * num_pos + [0] * num_neg
-    pred_scores = np.zeros(len(target_nodes), dtype=float)
+for idx in tqdm(val_sample_index):
+    source = int(valid_edge["source_node"][idx])
+    target = int(valid_edge["target_node"][idx])
+    target_neg = valid_edge["target_node_neg"][idx].tolist()
 
-    for idx, node in enumerate(target_nodes):
+    try:
+        pos_score = np.dot(embedding[source], embedding[target])
+    except:
+        pos_score = 0.0
+    pos_scores.append(pos_score)
+
+    neg_score = list()
+    for ix, node in enumerate(target_neg):
         try:
-            #pred_scores[idx] += np.dot(embedding[source], embedding[node])
-            pred_scores[idx] += torch.dot(embedding[source], embedding[node]).to(device).tolist()
+            score = np.dot(embedding[source], embedding[node])
         except:
-            continue
+            score = 0.0
+        neg_score.append(score)
+    neg_scores.append(neg_score)
 
-    y_pred = ["N"] * len(pred_scores)
-    sorted_indexes = np.argsort(-pred_scores)
-
-    for rank, idx in enumerate(sorted_indexes):
-        if idx < num_pos:
-            y_pred[rank] = 1
-        else:
-            y_pred[rank] = 0
-
-    rs = np.asarray(y_pred).nonzero()[0]
-    val_mrrs.append(1. / (rs[0] + 1) if rs.size else 0.)
+val_result = evaluator.eval({"y_pred_pos": torch.tensor(pos_scores), "y_pred_neg": torch.tensor(neg_scores)})
 
 # Predict test set
 print("Predicting testing set....")
+pos_scores = list()
+neg_scores = list()
 
-for key in tqdm(test_sampled_keys):
-    source = int(key)
-    target_nodes = [test_edges[key]["pos_node"]] + test_edges[key]["neg_node"]
-    
-    y_true = [1] * num_pos + [0] * num_neg
-    pred_scores = np.zeros(len(target_nodes), dtype=float)
+for idx in tqdm(test_sample_index):
+    source = int(test_edge["source_node"][idx])
+    target = int(test_edge["target_node"][idx])
+    target_neg = test_edge["target_node_neg"][idx].tolist()
 
-    for idx, node in enumerate(target_nodes):
+    try:
+        pos_score = np.dot(embedding[source], embedding[target])
+    except:
+        pos_score = 0.0
+    pos_scores.append(pos_score)
+
+    neg_score = list()
+    for ix, node in enumerate(target_neg):
         try:
-            #pred_scores[idx] += np.dot(embedding[source], embedding[node])
-            pred_scores[idx] += torch.dot(embedding[source], embedding[node]).to(device).tolist()
+            score = np.dot(embedding[source], embedding[node])
         except:
-            continue
+            score = 0.0
+        neg_score.append(score)
+    neg_scores.append(neg_score)
 
-    y_pred = ["N"] * len(pred_scores)
-    sorted_indexes = np.argsort(-pred_scores)
-
-    for rank, idx in enumerate(sorted_indexes):
-        if idx < num_pos:
-            y_pred[rank] = 1
-        else:
-            y_pred[rank] = 0
-
-    rs = np.asarray(y_pred).nonzero()[0]
-    test_mrrs.append(1. / (rs[0] + 1) if rs.size else 0.)
+test_result = evaluator.eval({"y_pred_pos": torch.tensor(pos_scores), "y_pred_neg": torch.tensor(neg_scores)})
 
 print("===== Prediction Result =====")
-print("Valid MRR: {:.4f}".format(np.mean(val_mrrs)))
-print("Test MRR: {:.4f}".format(np.mean(test_mrrs)))
+print("Valid MRR: {:.4f}".format(float(torch.mean(val_result["mrr_list"]))))
+print("Test MRR: {:.4f}".format(float(torch.mean(test_result["mrr_list"]))))
 
