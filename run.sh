7b46ff3cee08181b9a2aec38f3f7effcc0d428c0
Chi-Hung Chang <chihung861224@gmail.com>
2021-09-09 14:43:40 +0800

    modify HPE execute file name

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 python3 SampleEdges.py --dataset ogbl-citation2 --train_network network.txt --train_percent 100
 #smore/cli/hpe -train network.txt -save hpe_rep.txt -dimensions 64 -undirected 0 -sample_times 1200 -walk_steps 5 -threads 8
-./hpe -train network.txt -embed node-feat.txt -save hpe_rep.txt -dimensions 64 -undirected 0 -sample_times 1200 -walk_steps 5 -threads 8
+./HPE -train network.txt -embed node-feat.txt -save hpe_rep.txt -dimensions 64 -undirected 0 -sample_times 1200 -walk_steps 5 -threads 8
 python3 predict.py --dataset ogbl-citation2 --val_percent 100 --test_percent 100 --embed hpe_rep.txt
 
