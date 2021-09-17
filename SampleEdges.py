4a94885fd739360a0bc08d10a086760cf14dedbd
Chi-Hung Chang <chihung861224@gmail.com>
2021-09-17 11:06:04 +0800

    generate doc of year papers published

--- SampleEdges.py
+++ SampleEdges.py
@@ -11,28 +11,36 @@ parser = argparse.ArgumentParser(description="OGB Dataset")
 parser.add_argument("--dataset", type=str, default="ogbl-citation2")
 parser.add_argument("--train_network", type=str, default="network.txt")
 parser.add_argument("--train_percent", type=int, default=100)
-parser.add_argument("--directed", type=bool, default=False)
+parser.add_argument("--tag", type=bool, default=False)
 args = parser.parse_args()
 
 # Save arguments to variables
 dataset_name = args.dataset
 train_network = args.train_network
 train_percent = args.train_percent
-directed = args.directed
+tag = args.tag
 
 # Load dataset from OGB
 dataset = PygLinkPropPredDataset(name=dataset_name)
 feature = dataset[0]["x"]
+years = dataset[0]["node_year"]
 split_edge = dataset.get_edge_split()
 train_edge = split_edge["train"]
+
+# Generate meta-data (year paper published)
+print("Generate dataset meta")
+
+with open("node-year.txt", "w") as f:
+    for idx, year in tqdm(enumerate(years)):
+        f.write("{} {}\n".format(idx + 1, int(year[0])))
 
 # Generate build-in features
 print("Generate build-in features file")
 
 with open("node-feat.txt", "w") as f:
     for row in tqdm(feature):
-        for feature in row:
-            f.write("{} ".format(round(float(feature), 6)))
+        for x in row:
+            f.write("{} ".format(round(float(x), 6)))
         f.write("\n")
 
 # Sample training network
@@ -56,7 +64,7 @@ with open(train_network, "w") as f:
         source = edge[0]
         target = edge[1]
 
-        if directed == True:
+        if tag == True:
             f.write("S{} T{} 1\n".format(source, target))
         else:
             f.write("{} {} 1\n".format(source, target))
