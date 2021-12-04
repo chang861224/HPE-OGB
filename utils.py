4eb0420a7a9ec77fd3ffee36f60c2e69ab3117b3
Chi-Hung Chang <chihung861224@gmail.com>
2021-12-04 22:00:17 +0800

    fix bug

--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def SplitDataset(dataset, percent=10):
 
     for node in sample_instances:
         sample_set["source"].append(node)
-        sample_set["pos_target"].append(random.sample(edgelist[node], 1))
+        sample_set["pos_target"].append(random.sample(edgelist[node], 1)[0])
         sample_set["neg_target"].append(random.sample(all_nodes - set(edgelist[node]) - {node}, 1000))
 
     return sample_set
@@ -117,23 +117,12 @@ def _eval_mrr(y_pred_pos, y_pred_neg, type_info):
         y_pred_pos is an array with shape (batch size, )
     '''
 
-
     if type_info == 'torch':
         y_pred = torch.cat([y_pred_pos.view(-1,1), y_pred_neg], dim = 1)
         argsort = torch.argsort(y_pred, dim = 1, descending = True)
         ranking_list = torch.nonzero(argsort == 0, as_tuple=False)
         ranking_list = ranking_list[:, 1] + 1
-#hits1_list = (ranking_list <= 1).to(torch.float)
-#hits3_list = (ranking_list <= 3).to(torch.float)
-#hits10_list = (ranking_list <= 10).to(torch.float)
         mrr_list = 1./ranking_list.to(torch.float)
-       
-        """ 
-        return {'hits@1_list': hits1_list,
-                 'hits@3_list': hits3_list,
-                 'hits@10_list': hits10_list,
-                 'mrr_list': mrr_list}
-        """
         return mrr_list
 
     else:
@@ -141,22 +130,11 @@ def _eval_mrr(y_pred_pos, y_pred_neg, type_info):
         argsort = np.argsort(-y_pred, axis = 1)
         ranking_list = (argsort == 0).nonzero()
         ranking_list = ranking_list[1] + 1
-#hits1_list = (ranking_list <= 1).astype(np.float32)
-#hits3_list = (ranking_list <= 3).astype(np.float32)
-#hits10_list = (ranking_list <= 10).astype(np.float32)
         mrr_list = 1./ranking_list.astype(np.float32)
-    
-        """
-        return {'hits@1_list': hits1_list,
-                 'hits@3_list': hits3_list,
-                 'hits@10_list': hits10_list,
-                 'mrr_list': mrr_list}
-        """
         return mrr_list
     
 
 if __name__ == "__main__":
-    pos = [0.3, 1]
-    neg = [[0.5, 0.11, 0.2, 0.8], [0.3, 0.5, 0.1, 0.9]]
-    print("%.4f" % float(torch.mean(Evaluate({"y_pred_pos": torch.tensor(pos), "y_pred_neg": torch.tensor(neg)}))))
+    test_set = SplitDataset("pubmed", 20)
+    print(len(test_set["source"]))
 
